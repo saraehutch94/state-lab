9fa3204139cc982883143f29aa5ad41f76264d24
saraehutch94 <84148986+saraehutch94@users.noreply.github.com>
2021-12-22 12:28:53 -0500

    take out helper comments to clean up code

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import "./styles.css";
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
 
 export default function App() {
-  // USE useState TO CREATE  [bigImage, setBigImage]
-  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
-
   const [bigImage, setBigImage] = useState(imagesArr[0].img);
-
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
 
   const handleClick = (img) => {
     setBigImage(img);
   };
-
-  // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
-  // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
 
   const images = imagesArr.map((object, index) => {
     return (
@@ -37,8 +28,6 @@ export default function App() {
       <h1>Cities Of The World</h1>
       <div id="wrapper">
         <div id="thumbnails">{images}</div>
-        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
         <img src={bigImage} id="bigimage" alt="bigImage" />
       </div>
     </div>
